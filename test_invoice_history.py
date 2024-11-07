8ed2b65b7b6d342b080d10c53324522f8be78d1a
Federico Ragona <federico.ragona@gmail.com>
2024-11-08 00:11:14 +0100

    feat: download all invoices

--- test_invoice_history.py
+++ test_invoice_history.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 import json
+from time import sleep
 from playwright.sync_api import sync_playwright
 from typing import List
 
@@ -12,20 +13,15 @@ class Invoice(BaseModel):
     amount: str
 
 
-def test_download_invoices():
-    with sync_playwright() as p:
-        browser = p.chromium.connect_over_cdp("http://localhost:9222")
-        default_context = browser.contexts[0]
-        page = default_context.pages[0]
-        page.goto("https://mywheels.nl/mijn/financien", timeout=10000)
-        print("")
+def get_invoices(page) -> list[Invoice]:
+    table_selector = "div.mb-12:nth-child(2) > div:nth-child(1) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(2)"
+    page.wait_for_selector(table_selector, timeout=5000)
 
-        invoices: List[Invoice] = []
-        table_selector = "div.mb-12:nth-child(2) > div:nth-child(1) > div:nth-child(2) > table:nth-child(1) > tbody:nth-child(2)"
-        page.wait_for_selector(table_selector, timeout=5000)
-        for row in page.locator(f"{table_selector} > tr").all():
-            for col in row.locator("td").all():
-                values = col.all_inner_texts()[0].split("\n")
+    invoices: List[Invoice] = []
+    for row in page.locator(f"{table_selector} > tr").all():
+        for col in row.locator("td").all():
+            for text in col.all_inner_texts():
+                values = text.split("\n")
                 if len(values) == 2:
                     if values[0] == "#":
                         number = int(values[1])
@@ -33,15 +29,47 @@ def test_download_invoices():
                         issue_date = datetime.strptime(values[1], "%d-%m-%Y").date()
                     elif values[0] == "Totaalbedrag":
                         amount = values[1]
-            invoices.append(
-                Invoice(
-                    number=number,
-                    issue_date=issue_date,
-                    amount=amount
-                )
+        invoices.append(
+            Invoice(
+                number=number,
+                issue_date=issue_date,
+                amount=amount
             )
+        )
+    return invoices
+
+
+def test_download_invoices():
+    with sync_playwright() as p:
+        browser = p.chromium.connect_over_cdp("http://localhost:9222")
+        default_context = browser.contexts[0]
+        page = default_context.pages[0]
+        page.set_default_timeout(10000)
+        page.goto("https://mywheels.nl/mijn/financien", timeout=10000)
+        print("")
+
+        list_selector = "div.mb-12:nth-child(2) > div:nth-child(1) > ul:nth-child(3)"
+        page.wait_for_selector(list_selector, timeout=5000)
+        list_locator = page.locator(list_selector)
+        list_item_locator = list_locator.locator("li")
+
+        next = 1
+        last = int(list_item_locator.last.text_content())
+        print("last", last)
+
+        all_invoices: list[Invoice] = get_invoices(page)
+        print(f"downloaded invoices for page 1")
+
+        while next < last:
+            page.wait_for_selector(list_selector, timeout=5000)
+            next = int(list_item_locator.locator(".bg-blue").text_content()) + 1
+
+            list_item_locator.get_by_text(str(next), exact=True).click()
+            sleep(1)
+            all_invoices = all_invoices + get_invoices(page)
+            print(f"downloaded invoices for page {next}")
 
         with open("invoices.json", "w") as f:
-            f.write(json.dumps(invoices, indent=2, default=to_jsonable_python))
+            f.write(json.dumps(all_invoices, indent=2, default=to_jsonable_python))
 
         browser.close()
